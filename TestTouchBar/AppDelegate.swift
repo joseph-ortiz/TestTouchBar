dc958f149239703040356e9ccb184ad97412c63d
Joe Ortiz <josepho011235@gmail.com>
2017-03-04 14:33:59 -0500

    Initial Commit

---
+++ TestTouchBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TestTouchBar
+//
+//  Created by Joseph Ortiz on 3/4/17.
+//  Copyright © 2017 Joseph Ortiz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TestTouchBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestTouchBar
+//
+//  Created by Joseph Ortiz on 3/4/17.
+//  Copyright © 2017 Joseph Ortiz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
